db3d927f43e79f58e4ab5e3d0daabd6ebcc494d4
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-03-03 14:09:53 +0530

    2024.03.03

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,45 @@
+import pro1 from "../assets/project1.png";
+import pro2 from "../assets/project2.png";
+import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.png";
+import pro5 from "../assets/project5.png";
+
+
+const ProjectCardData=[
+    {
+        imgsrc: pro1,
+        title:"Portfolio web site design",
+        text: "This a website that developed using reactjs.A portfolio website is a digital platform where individuals or businesses showcase their work, projects, skills, and experiences.",
+        view: "https://github.com/dilanranasinghe96/portfolio"
+    },
+
+    {
+        imgsrc: pro2,
+        title:"E-commerce app",
+        text: "An application developed using flutter framework.That allows people to buy and sell physical goods, services over the internet rather than at a brick-and-mortar location.",
+        view: "https://github.com/dilanranasinghe96/E-Commerce-App"
+    },
+
+    {
+        imgsrc: pro3,
+        title:"Lawbo app",
+        text: "LawBo is a mobile application that created using flutter framework and using this application, we can know the criminal and traffic rules and more information about the rules.",
+        view: "https://github.com/Altier-Tech/lawbo-front-end-mobile"
+    },
+
+    {
+        imgsrc: pro4,
+        title:"GPA Generator app",
+        text: "GPA Generator is a mobile application that developed using flutter framework.The GPA generator application is a tool that helps students calculate their Grade Point Average quickly and accurately. ",
+        view: "https://github.com/dilanranasinghe96/GPA-Generator"
+    },
+
+    {
+        imgsrc: pro5,
+        title:"Facebook ui",
+        text: "The Facebook UI is a user interface (UI) design inspired by the layout and functionality of the Facebook app. This is created using flutter framework and it's look is same as a facebook application.",
+        view: "www.youtube.com"
+    }
+];
+
+export default ProjectCardData;
